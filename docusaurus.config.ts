ede1e43d102f094c6273ddbbd7184004228e961f
Emirhan Durmuş <emirhan.durmus@datasance.com>
2024-11-04 20:43:10 +0300

    base url fixed

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://alpaslan-datasance.github.io',
+  url: 'https://docs.datasance.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/datasanceDocusaurus/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
